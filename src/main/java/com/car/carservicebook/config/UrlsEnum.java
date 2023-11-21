8fe093cae15dfdce3545d2c84a2c4ff78d657709
Konn25 <koncz.bence9@gmail.com>
2023-11-21 12:07:23 +0100

    modify url

--- src/main/java/com/car/carservicebook/config/UrlsEnum.java
+++ src/main/java/com/car/carservicebook/config/UrlsEnum.java
@@ -7,7 +7,7 @@ public enum UrlsEnum {
 
     USER("/v1/user/data","/v1//user/update", "/v1/car/all/{userId}", "/v1/car/{id}",
             "/v1/car/new","/v1/car/update","/v1/car/picture/{id}", "/v1/car/refuel/{carId}", "/v1/car/refuel/new",
-            "/v1/car/refuel/update", "/v1/car/refuel/delete/{id}", "/v1/car/repair/{carId}", "/v1/car/repair/new", "/v1/car/repair/delete/{id}",
+            "/v1/car/refuel/update/{id}", "/v1/car/refuel/delete/{id}", "/v1/car/repair/{carId}", "/v1/car/repair/new", "/v1/car/repair/delete/{id}",
             "/v1/car/repair/update/{id}", "/v1/repair/name/new", "/v1/repair/name"
     ),
 
