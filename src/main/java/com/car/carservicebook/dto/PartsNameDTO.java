7f1e3afe24d2bb6f473181daed2336651c7b34c3
Konn25 <koncz.bence9@gmail.com>
2024-07-02 10:26:21 +0200

    add dto

---
+++ src/main/java/com/car/carservicebook/dto/PartsNameDTO.java
@@ -0,0 +1,20 @@
+package com.car.carservicebook.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PartsNameDTO {
+
+    @JsonIgnore
+    Long id;
+
+    String name;
+
+}
